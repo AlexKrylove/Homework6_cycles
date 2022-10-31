d4795bce5280bab7232880516ba0d05fb2a12080
AlexKrylov <krylofffff93@gmail.com>
2022-10-31 19:52:13 +0300

    Task2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Task");
+        System.out.println("Task1");
         for (int a = 0; a <= 10; a++) {
+            System.out.println("Iteration: " + a);
+        }
+        System.out.println("Task2");
+        for (int a = 10; a >= 0; a--) {
             System.out.println("Iteration: " + a);
         }
     }
