11e2b5c125d56fa0b64f30f8415252d4be7be041
AlexKrylov <krylofffff93@gmail.com>
2022-10-31 20:07:07 +0300

    Task4

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,11 @@ public class Main {
         for (int number = 2; number <= 17;number+=2) {
             System.out.println("Четное число:" +  number);
         }
+        System.out.println("Task4");
+        for (int number = 10; number >= -10; number--) {
+            System.out.println("В убывающем порядке " + number);
+        }
     }
+
 
 }
