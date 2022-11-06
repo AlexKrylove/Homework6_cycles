63d876323313e17cda88e165a4d18f4c22d1958c
AlexKrylov <krylofffff93@gmail.com>
2022-11-07 00:40:24 +0300

    Task 3 population сделано

--- src/DOWhile.java
+++ src/DOWhile.java
@@ -1,25 +1,6 @@
 public class DOWhile {
     public static void main(String[] args) {
-        System.out.println("Task_While_1");
-        int saving = 15000;
-        int sum = 0;
-        int i = 0;
-        while (sum <= 2459000) {
-            sum = sum + saving+saving/100;
-            i++;
-          System.out.println("Месяц " + i + " сумма накоплений равна "+ sum);
-        }
-        System.out.println("Task_While_2");
-        int a = 0;
-        while (a<10) {
-            a++;
-            System.out.print(a + " ");
-        }
-        System.out.println(" ");
-        for (int b = 10; b>=1; b--){
-            System.out.print(b + " ");
-        }
-        System.out.println("Task_While_3");
+                System.out.println("Task_While_3");
         int population  = 12_000_000;
         int newBorn = 17;
         int deadPeople = 8;
@@ -28,7 +9,7 @@ public class DOWhile {
         while (year<=9){
             year++;
             population +=population*diff/1000;
-            System.out.println("Год " + year + " чиленность населения составит " + population);
+            System.out.println("Год " + year + " численность населения составит " + population);
         }
 
     }
